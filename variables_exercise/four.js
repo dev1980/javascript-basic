54d9079c46a94218662534523385f13e988fd9fd
dev1980 <brham1980@gmail.com>
2021-04-13 17:09:36 +0545

    variables exercise

---
+++ variables_exercise/four.js
@@ -0,0 +1,4 @@
+let movie = "Dragon for go";
+let views = 4;
+console.log("I watched " + movie + " about " + views + " number of times");
+console.log(`I watched ${movie} about ${views} number of times`);
