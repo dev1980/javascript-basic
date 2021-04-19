f76ba9c72f8935050fbb361757296fd129083627
dev1980 <brham1980@gmail.com>
2021-04-19 21:01:16 +0545

    function example continue

---
+++ A_functions_exercise/keep-it-quiet.js
@@ -0,0 +1,6 @@
+// Write a function `average` that accepts three numbers as arguments. The function should return the
+// average of the three numbers.
+
+console.log(average(3, 10, 8)); // 7
+console.log(average(10, 5, 12)); // 9
+console.log(average(6, 20, 40)); // 22
