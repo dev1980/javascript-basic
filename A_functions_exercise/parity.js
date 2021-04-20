6d61ce3c809b9d37227b11f592dbac6d0633bdf6
dev1980 <brham1980@gmail.com>
2021-04-20 21:31:08 +0545

    continue with function example

--- A_functions_exercise/parity.js
+++ A_functions_exercise/parity.js
@@ -1,6 +1,13 @@
 // Write a function `parity` that accepts a number as an argument. The function should return the
 // string 'even' if the number is even. It should return the string 'odd' if the number is odd.
 
+function parity(num) {
+  if (num % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
 console.log(parity(5)); // 'odd'
 console.log(parity(7)); // 'odd'
 console.log(parity(13)); // 'odd'
