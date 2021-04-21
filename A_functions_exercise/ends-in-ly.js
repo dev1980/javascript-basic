2b71a61078169adc86d1026b0f6da6d48e1e8413
dev1980 <brham1980@gmail.com>
2021-04-21 22:01:12 +0545

    function examples continue

---
+++ A_functions_exercise/ends-in-ly.js
@@ -0,0 +1,13 @@
+// Write a function `endsInLy` that accepts a string as an argument and returns a boolean indicating
+// whether or not the string ends in the substring 'ly'.
+function endsInLy(str) {
+  let secondLast = str[str.length - 2];
+  let last = str[str.length - 1];
+  return secondLast==="l" &&& last === "y"
+}
+console.log(endsInLy("pretty")); // false
+console.log(endsInLy("instant")); // false
+console.log(endsInLy("analytic")); // false
+console.log(endsInLy("timidly")); // true
+console.log(endsInLy("fly")); // true
+console.log(endsInLy("gallantly")); // true
