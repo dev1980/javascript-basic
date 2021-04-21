e685709fca113cb943ac77ce5734a322e026b15e
dev1980 <brham1980@gmail.com>
2021-04-21 21:44:38 +0545

    javascript example

--- A_functions_exercise/starts-with-r.js
+++ A_functions_exercise/starts-with-r.js
@@ -1,5 +1,12 @@
 // Write a function `startsWithR` that accepts a string as an argument and returns a boolean indicating
 // whether or not the string starts with 'r' or 'R'.
+function startsWithR(str) {
+  if (str[0] === "r" || str[0] === "R") {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 console.log(startsWithR("roger that")); // true
 console.log(startsWithR("Row, row, row your boat")); // true
