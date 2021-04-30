439e9f7991c43866701cd57e35ac64491f28dd06
dev1980 <brham1980@gmail.com>
2021-04-30 22:50:38 +0545

    no-ohs example

---
+++ B_loops/no-ohs.js
@@ -0,0 +1,24 @@
+// Write a function named `noOhs` that accepts a string as an argument. The functions should print the
+// characters of the string one by one except the character 'o'. The function doesn't need to return
+// any value. It should just print to the terminal.
+
+function noOhs(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] != "o") {
+      console.log(str[i]);
+    }
+  }
+}
+
+noOhs("code");
+// prints
+//  c
+//  d
+//  e
+
+noOhs("school");
+// prints
+//  s
+//  c
+//  h
+//  l
