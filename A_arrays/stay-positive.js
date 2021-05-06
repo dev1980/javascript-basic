57aaf1a28da4f4526aabab4ca5510f5a27fb6183
dev1980 <brham1980@gmail.com>
2021-05-06 08:05:11 +0545

    array stayPositive solution

---
+++ A_arrays/stay-positive.js
@@ -0,0 +1,16 @@
+// Write a function `stayPositive` that accepts an array of numbers as an argument. The function should
+// return an array containing only the positive numbers.
+
+function stayPositive(numbers) {
+  let arr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+      arr.push(numbers[i]);
+    }
+  }
+  return arr;
+}
+
+console.log(stayPositive([10, -4, 3, 6])); // [10, 3, 6]
+console.log(stayPositive([-5, 11, -40, 30.3, -2])); // [11, 30.3]
+console.log(stayPositive([-11, -30])); // []
