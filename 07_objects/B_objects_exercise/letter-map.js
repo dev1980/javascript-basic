1d85197afad84a7b50cbbe52149140eaad0ac703
Brham Dev Mahato <brham1980@gmail.com>
2021-06-04 21:44:54 +0545

    letterMap

---
+++ 07_objects/B_objects_exercise/letter-map.js
@@ -0,0 +1,26 @@
+// Write a function `letterMap` that accepts a string and an object as arguments. The function should
+// return a version of the string where characters that appear as keys in object are replaced with
+// their corresponding values.
+
+function letterMap(str, obj) {
+let result = ""
+let k = Object.keys(obj)
+for(let i = 0; i < str.length; i++) {
+    let char = str[i]
+    for(let j = 0; j < k.length; j++) {
+        let ch = k[j]
+        if(char === ch){
+            result += obj[ch]
+        } else {
+            result += char
+        }
+    }
+   
+}
+return result
+}
+
+
+console.log(letterMap("symbolic", { y: "i", o: "a", c: "k" })); // 'simbalik'
+console.log(letterMap("colossal", { o: "x", s: "p" })); // 'cxlxppal'
+console.log(letterMap("miniscule", { u: "t", i: "f", e: "q" })); // 'mfnfsctlq'
