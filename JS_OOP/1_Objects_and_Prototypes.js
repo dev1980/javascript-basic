eb41bae126e0df3a0ab2d64b96a549dd1d465883
Brham Dev Mahato <brham1980@gmail.com>
2021-06-18 21:44:07 +0545

    different way to call the function

--- JS_OOP/1_Objects_and_Prototypes.js
+++ JS_OOP/1_Objects_and_Prototypes.js
@@ -58,4 +58,83 @@ console.log(emp2)
 console.log(emp3)
 
 // this way of creating object in javascript is very common in javascript,
-//but language provide shortcut way to write the same code.
+//but language provide shortcut way to write the same code using constructor way.
+
+function Employee(firstName, lastName, gender, degination) {
+    // var newObject = {}
+    this.fname = firstName;
+    this.lname = lastName;
+    this.gender = gender;
+    this.degination = degination;
+    // return newObject
+}
+
+var emp4 = new Employee("Hari", "krishna", "male", "Driver")
+
+
+// Threre is total 4 way of calling function
+
+function foo() {
+    console.log("Hello");
+    console.log(this)
+}
+
+foo(); // this is first way of calling function in javascript , Method 1
+// value of this is referance to global object. i.e window
+
+var obj = {"name": "Brham Dev"};
+obj.foo = function() {
+    console.log("Hello from object");
+    console.log(this)
+
+}
+
+
+obj.foo() // function is the propety of object. Mehtod 2
+
+// if we calling the function in context of object the this refer to the object whose property is the function.
+
+// executing fuction in constrcution way , Method 3 , new foo()
+
+// Lets talk aboaut the execution concept in Javascript. this is the concept in most programming language.
+// when function is called it always called in certain context. execution context.
+
+//The value of this is always depends on the function call.
+
+
+// new foo()
+
+// Here this refer to the new object it create or first it will be empty object
+
+function foo() {
+    // var this = {}
+    console.log("Hello");
+    console.log(this)
+    // return this
+}
+
+new foo();
+
+// here u will get empty object.
+
+function Bicycle(cadance, speed, gear, tirePressure) {
+this.cadance = cadance;
+this.speed = speed;
+this.gear = gear;
+this.tirePressure = tirePressure;
+this.inflateTire = function(){
+    this.tirePressure += 3;
+}
+
+}
+
+let bicycle1 = new Bicycle(50,20,4,25);
+bicycle1.inflateTire();
+
+function Mechanic(name){
+this.name = name;
+}
+
+var mike = new Mechanic("Mike");
+
+mike.inflateTire.call(bicycle1)
