ea29377033502b0068a27a1200740c6b8c5040f8
Brham Dev Mahato <brham1980@gmail.com>
2021-06-23 21:46:20 +0545

    callback example

--- JS_OOP/callback_function.js
+++ JS_OOP/callback_function.js
@@ -1,5 +1,16 @@
 // What is call back function in javascript
 
+setTimeout(function(){
+    console.log("timer")
+}, 5000)
+
+function y(x){
+console.log("y")
+}
+y(function x(){
+console.log("x")
+})
+
 //Javascript is a synchronous and single-threaded language
 
 // Blocking and main thread
