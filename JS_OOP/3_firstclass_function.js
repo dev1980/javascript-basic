6625574d6b91d6096caec6f98f84902048b7d46d
Brham Dev Mahato <brham1980@gmail.com>
2021-06-24 21:44:38 +0545

    hoisting

--- JS_OOP/3_firstclass_function.js
+++ JS_OOP/3_firstclass_function.js
@@ -1,5 +1,30 @@
 //What are first class function ?
 
-//What is Anonymous Functions
+//What is Anonymous Functions ( function with no name we call it anonymous function.)
 
-// function with no name we call it anonymous function.
+
+
+//Function statement 
+
+function a() {
+    console.log("hello")
+}
+
+// Function Expression
+
+var b = function() {
+    console.log("display B")
+}
+
+// Difference between function statement and expression is Hoisting.
+
+//Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution. Inevitably, 
+//this means that no matter where functions and variables are declared, they are moved to the top of their scope regardless of whether their scope is global or local
+
+
+//Function Declaration ?
+
+
+
+
+
